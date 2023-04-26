44bae37cfde62a61bb42428b763d6bf37ec208c2
mgsturdy <88903078+mgsturdy@users.noreply.github.com>
2023-04-26 11:47:03 -0700

    Update LocationViewController.swift

--- LocationViewController.swift
+++ LocationViewController.swift
@@ -2,17 +2,20 @@ import UIKit
 import MapKit
 import CocoaMQTT
 import CoreLocation
+import UserNotifications
 
 class LocationViewController: UIViewController, CLLocationManagerDelegate {
 
     @IBOutlet weak var mapView: MKMapView!
     private var mqtt: CocoaMQTT!
     private let locationManager = CLLocationManager()
+    private var geofenceRegion: CLCircularRegion?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         setupLocationManager()
         setupMQTT()
+        requestNotificationPermission()
     }
 
     private func setupLocationManager() {
@@ -30,6 +33,7 @@ class LocationViewController: UIViewController, CLLocationManagerDelegate {
         mqtt.delegate = self
         mqtt.connect()
     }
+    
 
     private func updateMapLocation(latitude: Double, longitude: Double) {
         let location = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
@@ -39,12 +43,60 @@ class LocationViewController: UIViewController, CLLocationManagerDelegate {
         let annotation = MKPointAnnotation()
         annotation.coordinate = location
         mapView.addAnnotation(annotation)
+        
+        if let geofenceRegion = geofenceRegion {
+            if geofenceRegion.contains(location) == false {
+                locationManager(manager: locationManager, didExitRegion: geofenceRegion)
+            }
+        }
     }
 
-    // CLLocationManagerDelegate method
+    private func setupGeofence(latitude: CLLocationDegrees, longitude: CLLocationDegrees, radius: CLLocationDistance) {
+        let geofenceCenter = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        geofenceRegion = CLCircularRegion(center: geofenceCenter, radius: radius, identifier: "Geofence")
+        geofenceRegion?.notifyOnExit = true
+    }
+
+    private func requestNotificationPermission() {
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound]) { granted, error in
+            if let error = error {
+                print("Error requesting notification permission: \(error.localizedDescription)")
+            }
+        }
+    }
+
+    private func sendGeofenceNotification() {
+        let content = UNMutableNotificationContent()
+        content.title = "Geofence Alert"
+        content.body = "MQTT device has left the geofence area!"
+        content.sound = .default
+
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 1, repeats: false)
+        let request = UNNotificationRequest(identifier: "GeofenceNotification", content: content, trigger: trigger)
+
+        UNUserNotificationCenter.current().add(request) { error in
+            if let error = error {
+                print("Error sending geofence notification: \(error.localizedDescription)")
+            } else {
+                print("Geofence notification sent.")
+            }
+        }
+    }
+
+        // CLLocationManagerDelegate methods
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         if let location = locations.last {
-            // Handle location updates, e.g., send location to another device via MQTT
+            if geofenceRegion == nil {
+                let geofenceRadius: CLLocationDistance = 100 // radius in meters
+                setupGeofence(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude, radius: geofenceRadius)
+            }
+            // Handle other location updates, e.g., send location to another device via MQTT
+        }
+    }
+
+    func locationManager(_ manager: CLLocationManager, didExitRegion region: CLRegion) {
+        if region.identifier == "Geofence" {
+            sendGeofenceNotification()
         }
     }
 }
@@ -70,3 +122,4 @@ extension LocationViewController: CocoaMQTTDelegate {
         }
     }
 }
+
