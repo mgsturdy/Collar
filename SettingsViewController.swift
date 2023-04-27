13c2c5c6b18bbe10ec93bd6b0b9cfcf02a32b316
mgsturdy <88903078+mgsturdy@users.noreply.github.com>
2023-04-27 13:38:09 -0700

    Update and rename SettingsViewController.swift to SceneDelegate.swift

--- SettingsViewController.swift
+++
@@ -1,9 +0,0 @@
-import UIKit
-
-class SettingsViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Add your settings related code here
-    }
-}
